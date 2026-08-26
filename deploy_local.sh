16e838f109729491f25d7db8e2add6f32b5ddf44
isln <isln2027@yandex.ru>
2026-08-26 21:37:09 +0300

    Prepare local deployment

---
+++ deploy_local.sh
@@ -0,0 +1,10 @@
+ ./mvnw clean package
+
+path_to_tomcat="${1:-../apache-tomcat}"
+cp ./target/yp-blog.war ${path_to_tomcat}/webapps/yp-blog.war
+
+chmod +x ${path_to_tomcat}/bin/*.sh
+echo "Shutdown container"
+${path_to_tomcat}/bin/shutdown.sh > /dev/null 2>&1
+echo "Starting container"
+${path_to_tomcat}/bin/startup.sh
