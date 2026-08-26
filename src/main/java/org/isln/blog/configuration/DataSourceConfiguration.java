783525e6f6d98554b6262e7d206f7ee6fd83a9e5
isln <isln2027@yandex.ru>
2026-08-26 22:28:41 +0300

    Create tables on app start
    Fix schema
    
    Fix model

--- src/main/java/org/isln/blog/configuration/DataSourceConfiguration.java
+++ src/main/java/org/isln/blog/configuration/DataSourceConfiguration.java
@@ -2,6 +2,8 @@ package org.isln.blog.configuration;
 
 import javax.sql.DataSource;
 
+import lombok.RequiredArgsConstructor;
+
 import org.h2.Driver;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -9,29 +11,43 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 @Configuration
+@RequiredArgsConstructor
 public class DataSourceConfiguration {
+    @Value("${spring.datasource.url}")
+    private final String url;
+    @Value("${spring.datasource.username}")
+    private final String username;
+    @Value("${spring.datasource.password}")
+    private final String password;
+    @Value("classpath:schema.sql")
+    private final Resource schema;
+
     @Bean
-    public DataSource dataSource(
-            @Value("${spring.datasource.url}") String url,
-            @Value("${spring.datasource.username}") String username,
-            @Value("${spring.datasource.password}") String password
-    ) {
+    public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(Driver.class.getName());
         dataSource.setUrl(url);
         dataSource.setUsername(username);
         dataSource.setPassword(password);
-
         return dataSource;
     }
 
     @Bean
     public JdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new JdbcTemplate(dataSource);
+    }
+
+    @EventListener
+    public void populate(ContextRefreshedEvent event) {
+        DataSource dataSource = event.getApplicationContext().getBean(DataSource.class);
+        ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+        populator.addScript(schema);
+        populator.execute(dataSource);
     }
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE if NOT EXISTS posts(
+  id bigserial PRIMARY KEY,
+  title text NOT NULL,
+  text text NOT NULL,
+  like_count int NOT NULL DEFAULT 0,
+  tags JSON not null,
+  comment_count int NOT NULL DEFAULT 0
+ );
+
+CREATE TABLE if NOT EXISTS comments(
+  id bigserial PRIMARY KEY,
+  text text NOT NULL,
+  post_id bigint NOT NULL REFERENCES posts(id)
+ );
