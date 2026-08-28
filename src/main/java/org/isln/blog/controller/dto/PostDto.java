1490a9c22152ec4fdd5033d6312a4f98d6a65704
isln <isln2027@yandex.ru>
2026-08-28 11:55:23 +0300

    Prepare 'post' dtos

---
+++ src/main/java/org/isln/blog/controller/dto/PostDto.java
@@ -0,0 +1,19 @@
+package org.isln.blog.controller.dto;
+
+import java.util.Set;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+public class PostDto {
+    private Long id;
+    private String title;
+    private String text;
+    private Set<String> tags;
+    private Integer commentCount;
+    private Integer likeCount;
+}
