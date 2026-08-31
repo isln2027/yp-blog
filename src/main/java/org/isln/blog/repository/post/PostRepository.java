1907c9bba12c419323f454b26c36780071f1b4bc
isln <isln2027@yandex.ru>
2026-08-31 12:13:03 +0300

    Cleanup

--- src/main/java/org/isln/blog/repository/post/PostRepository.java
+++ src/main/java/org/isln/blog/repository/post/PostRepository.java
@@ -7,12 +7,20 @@ import org.isln.blog.model.Post;
 
 public interface PostRepository {
     Post findById(Long id);
+
     List<Post> find(Integer pageNumber, Integer pageSize, PostRequestParameters parameters);
+
     Long create(Post post);
+
     void update(Post post);
+
     void delete(Long id);
+
     void delete(Collection<Long> ids);
+
     Long count(PostRequestParameters parameters);
+
     Integer incrementLikeCount(Long id);
+
     Boolean exists(Long id);
 }
