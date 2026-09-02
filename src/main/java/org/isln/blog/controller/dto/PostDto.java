e97d741fab52eb305f5a10b8723d378614733ccf
isln <isln2027@yandex.ru>
2026-09-02 13:41:45 +0300

    Fix dtos to match required api

--- src/main/java/org/isln/blog/controller/dto/PostDto.java
+++ src/main/java/org/isln/blog/controller/dto/PostDto.java
@@ -6,8 +6,6 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 @Getter
 @Setter
 @Accessors(chain = true)
@@ -16,8 +14,6 @@ public class PostDto {
     private String title;
     private String text;
     private Set<String> tags;
-    @JsonProperty("comment_count")
     private Integer commentCount;
-    @JsonProperty("like_count")
     private Integer likeCount;
 }
