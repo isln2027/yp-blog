bfb76207b21cf3f436a3a9440128109f39df88fd
isln <isln2027@yandex.ru>
2026-09-02 11:25:08 +0300

    Fix dtos to match expected api

--- src/main/java/org/isln/blog/controller/dto/CommentDto.java
+++ src/main/java/org/isln/blog/controller/dto/CommentDto.java
@@ -4,11 +4,14 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 @Getter
 @Setter
 @Accessors(chain = true)
 public class CommentDto {
     private Long id;
     private String text;
+    @JsonProperty("post_id")
     private Long postId;
 }
