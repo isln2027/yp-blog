c3b264b7a6f9d421d854e59b90221dc01fa68205
isln <isln2027@yandex.ru>
2026-09-02 11:28:34 +0300

    Fix 'post' controller path

--- src/main/java/org/isln/blog/controller/PostController.java
+++ src/main/java/org/isln/blog/controller/PostController.java
@@ -18,11 +18,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping(value = "/api")
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
